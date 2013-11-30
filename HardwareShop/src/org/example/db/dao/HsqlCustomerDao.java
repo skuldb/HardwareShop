57cac17dbb3c287a5452fed7426ed19ea9f1cba2
skuldb <kamilpesta87@gmail.com>
2013-11-30 20:12:42 +0100

    Projekt

---
+++ HardwareShop/src/org/example/db/dao/HsqlCustomerDao.java
@@ -0,0 +1,37 @@
+package org.example.db.dao;
+
+import java.util.List;
+
+import org.example.shop.Customer;
+
+public class HsqlCustomerDao implements CustomerDao{
+
+        public void save(Customer ent) {
+                // TODO Auto-generated method stub
+                
+        }
+
+        public void delete(Customer ent) {
+                // TODO Auto-generated method stub
+                
+        }
+
+        public List<Customer> getAll() {
+                // TODO Auto-generated method stub
+                return null;
+        }
+
+        public Customer get(int id) {
+                // TODO Auto-generated method stub
+                return null;
+        }
+
+     
+        
+
+        public void setOrders(Customer c) {
+                // TODO Auto-generated method stub
+                
+        }
+
+}
